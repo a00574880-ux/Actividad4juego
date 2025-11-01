2f0184f184da1bd23885b9eac6c5b2898d7b8055
a00574880.ux <a00574880@tec.mx>
2025-10-31 18:00:16 -0600

    Corregir errores

--- pacman.py
+++ pacman.py
@@ -10,7 +10,22 @@ Exercises
 """
 
 from random import choice
-from turtle import *
+from turtle import (
+    onkey,
+    Turtle,
+    bgcolor,
+    clear,
+    up,
+    goto,
+    dot,
+    update,
+    ontimer,
+    setup,
+    hideturtle,
+    tracer,
+    listen,
+    done
+)
 
 from freegames import floor, vector
 
